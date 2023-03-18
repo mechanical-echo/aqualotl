61d540650b237b42319967c86b2475ce64b075c0
Sofija Dyshlovaya <sofija.dyshlovaya@gmail.com>
2023-03-18 20:16:59 +0200

    initialize git,made ui for login, welcome, signup

---
+++ lib/pages/HomePage/homepage.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, depend_on_referenced_packages, library_private_types_in_public_api
+
+import 'package:aqualotl/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget{
+   const HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
+ 
