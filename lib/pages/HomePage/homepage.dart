3bfdf6621b313ebc49c6685cba24018d5d55b1d9
Sofija Dyshlovaya <sofija.dyshlovaya@gmail.com>
2023-04-06 07:16:01 +0300

    add buttons are now working well

--- lib/pages/HomePage/homepage.dart
+++ lib/pages/HomePage/homepage.dart
@@ -18,15 +18,170 @@ class _HomePageState extends State<HomePage> {
   final triangleBottleRef = GlobalKey<TriangularBottleState>();
   var waterLevel = 0.5;
   var selectedStyle = 0;
-  int currentWaterLevel = 1520;
+  int currentWaterLevel = 0;
   final maxWaterLevel = 2700;
   double progress = 0;
-  void changeProgress() {
-    currentWaterLevel += 500;
+  double _progressValue = 0.0;
+
+  void _incrementProgressValue(int howMuch) {
     setState(() {
-      progress = currentWaterLevel / maxWaterLevel;
-      print(progress);
+      currentWaterLevel += howMuch;
+      print(currentWaterLevel.toString() + "/" + maxWaterLevel.toString());
+      print(currentWaterLevel / maxWaterLevel);
+      _progressValue = (currentWaterLevel / maxWaterLevel);
+      if (_progressValue > 1) {
+        _progressValue = 1;
+        currentWaterLevel = maxWaterLevel;
+      }
+      print(_progressValue);
     });
+  }
+
+  void _openInputDialog() {
+    String inputValue = '';
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Enter a value'),
+          content: TextFormField(
+            onChanged: (value) {
+              inputValue = value;
+            },
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              hintText: 'Enter a value to add',
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Add'),
+              onPressed: () {
+                int? valueToAdd = int.tryParse(inputValue);
+                _incrementProgressValue(valueToAdd!);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _openAddDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add'),
+          content: Text('Please, choose how much water must be added:'),
+          actions: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                _incrementProgressValue(250);
+                                Navigator.of(context).pop();
+                              },
+                              icon: const Image(
+                                image: AssetImage(glass),
+                              )),
+                          Text(
+                            "250ml  ",
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                _incrementProgressValue(300);
+                                Navigator.of(context).pop();
+                              },
+                              icon: const Image(
+                                image: AssetImage(cup),
+                              )),
+                          Text(
+                            "300ml  ",
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                _incrementProgressValue(500);
+                                Navigator.of(context).pop();
+                              },
+                              icon: const Image(
+                                image: AssetImage(measuring),
+                              )),
+                          Text(
+                            "500ml  ",
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                _openInputDialog();
+                                //_incrementProgressValue();
+                              },
+                              icon: const Image(
+                                image: AssetImage(edit),
+                              )),
+                          Text(
+                            "Other..",
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          TextButton(
+                            child: Text('Cancel'),
+                            style: Theme.of(context).textButtonTheme.style,
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            /** 
+            TextButton(
+              child: Text('Add'),
+              onPressed: () {
+                _incrementProgressValue();
+                Navigator.of(context).pop();
+              },
+            ),*/
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -72,23 +227,51 @@ class _HomePageState extends State<HomePage> {
                 children: [
                   WelcomeBox(phoneW: phoneW, phoneH: phoneH),
                   const Divider15(),
-                  ProgressCircle(phoneH: phoneH, progress: progress),
+                  ProgressCircle(phoneH: phoneH, progress: _progressValue),
                 ],
               ),
             ),
-            const Divider15(),
+            const Divider15Vertical(),
             /**
              * TIP OF THE DAY AND BUTTON
              */
             Row(
-              children: const [
-                Divider25(),
-                TipOfTheDay(),
-                Divider15(),
-                ButtonAndText()
+              children: [
+                const Divider25(),
+                const TipOfTheDay(),
+                const Divider15(),
+                Container(
+                  width: MediaQuery.of(context).size.width - 50 - 200 - 15,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.shade200,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: _openAddDialog,
+                          icon: const Icon(
+                            Icons.add_box_outlined,
+                            color: lSubTextColor,
+                            size: 40,
+                          ),
+                        ),
+                        Text(
+                          "Click to \nadd water",
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
-            const Divider15(),
+            const Divider15Vertical(),
             /**
              * AXOLOTL IMAGE
              */
@@ -99,6 +282,19 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class Divider15Vertical extends StatelessWidget {
+  const Divider15Vertical({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 15,
     );
   }
 }
@@ -120,38 +316,6 @@ class FooterImage extends StatelessWidget {
       child: const Padding(
         padding: EdgeInsets.all(25),
         child: Image(image: AssetImage(splash)),
-      ),
-    );
-  }
-}
-
-class ButtonAndText extends StatelessWidget {
-  const ButtonAndText({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width - 50 - 200 - 15,
-      height: 140,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.grey.shade200,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            AddWaterDialog(),
-            Text(
-              "Click to \nadd water",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          ],
-        ),
       ),
     );
   }
@@ -284,15 +448,9 @@ class WelcomeBox extends StatelessWidget {
             "Welcome!",
             style: Theme.of(context).textTheme.displayMedium,
           ),
-          /**
-           * 
-           */
           const SizedBox(
             height: 10,
           ),
-          /**
-           * 
-           */
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 12),
             child: Text(
