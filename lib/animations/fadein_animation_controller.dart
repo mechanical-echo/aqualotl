28354568a3ceb95d6da54aa2c5f663d078dae255
Sofia Dyshlovaya <105802387+mechanical-echo@users.noreply.github.com>
2023-04-13 00:35:22 +0300

    Update fadein_animation_controller.dart

--- lib/animations/fadein_animation_controller.dart
+++ lib/animations/fadein_animation_controller.dart
@@ -1,32 +1,27 @@
 // ignore_for_file: camel_case_types, prefer_const_constructors
 
+import 'package:aqualotl/pages/authentication/signup/auth.dart';
 import 'package:get/get.dart';
 
 import '../pages/authentication/welcomescreen.dart';
 
-
-
-
-class tFadeInAnimationController extends GetxController{
-
+class tFadeInAnimationController extends GetxController {
   static tFadeInAnimationController get find => Get.find();
-
 
   RxBool animate = false.obs;
 
-  Future startSplashAnimation() async{
+  Future startSplashAnimation() async {
     await Future.delayed(Duration(milliseconds: 1200));
     animate.value = true;
     await Future.delayed(Duration(milliseconds: 3000));
     animate.value = false;
     await Future.delayed(Duration(milliseconds: 2500));
-    Get.offAll(() => const WelcomeScreen());
+    Get.offAll(() => const Auth());
     //Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => WelcomeScreen()));
   }
 
-  Future startAnimation() async{
+  Future startAnimation() async {
     await Future.delayed(Duration(milliseconds: 500));
     animate.value = true;
-    
   }
 }
